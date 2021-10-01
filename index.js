e99d233db6fcfa97d51470eca401f8c266a0a50d
Jane <janechodance@gmail.com>
2021-10-01 18:42:12 -0400

    commit tree 8cb12038df95a73a3831cdcd28481af0134db4cc

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your code here
+function submitData() {
+fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name: "Jane",
+    email: "janechodance@gmail.com",
+  }),
+});
+}
